e0840a57b6c90bff00f436e3e1dcdccaaa77c14b
topestar <topestar@live.com>
2023-01-11 09:10:05 +0000

    README file for 0x0A-python-inheritance

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""reads lines"""
+"""Defines a file-appending function."""
 
 
-def read_lines(filename="", nb_lines=0):
-    lines = 0
-    with open(filename, mode='r', encoding="utf-8") as f:
-        for line in f:
-            lines += 1
-            print(line, end="")
-            if lines == nb_lines:
-                break
-
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
